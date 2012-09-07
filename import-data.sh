ecd30575a69711a8def794850e5076da1667d140
Aleš Kotnik <ales.kotnik@gmail.com>
2012-09-07 08:46:26 +0200

    Import script iterates over all json files in exampledata

--- import-data.sh
+++ import-data.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # http://wiki.apache.org/solr/UpdateJSON
-for file in atc.json ingredients.json drugs.json; do
+for file in exampledata/*.json; do
   curl "http://localhost:8983/solr/draagle/update/json?commit=true" \
     -H "Content-Type: text/plain; charset=utf-8" \
-    --data-binary @exampledata/$file
+    --data-binary @$file
 done
